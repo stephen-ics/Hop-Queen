622568af429b53cadb75415c25ecdfde72b3c628
stephen-ics <stephenni1234@gmail.com>
2022-11-12 14:14:00 -0500

    Major Changes
    Player Movement + Level 1 Layout + Wind, Mud, Slime + Level Transition

---
+++ My project (1)/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(index);
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
+}
