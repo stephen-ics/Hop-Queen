4c7549d355e5f2b0988c0cd2dc9f16f4debf4d64
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2022-12-07 17:18:31 -0500

    Documented scripts + adjusted arrows

--- Game/Assets/Scripts/Menu/pauseMenu.cs
+++ Game/Assets/Scripts/Menu/pauseMenu.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Object that open up pause menu while pausing the game
+/// </summary>
 public class pauseMenu : MonoBehaviour
 {
-
+    /// <summary>
+    /// Bool to determine if game is paused/esc key pressed
+    /// </summary>
     public static bool GameIsPaused = false;
-
+    /// <summary>
+    /// Unity UI element
+    /// </summary>
     public GameObject pauseMenuUI;
 
-    // Update is called once per frame
+    /// <summary>
+    /// Checks if the escape key was pressed every frame
+    /// </summary>
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -25,34 +34,40 @@ public class pauseMenu : MonoBehaviour
             }
         }
     }
-
+    /// <summary>
+    /// Resumes game if resume button pressed. Sets bool to false
+    /// </summary>
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-
+    /// <summary>
+    /// Pauses game if escape button hit. Freezes the time and sets bool to true 
+    /// </summary>
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-
+    /// <summary>
+    /// Unfreezes time and sends the user to the Menu scene. Sets bool to false
+    /// </summary>
     public void LoadMenu()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
         GameIsPaused = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
-
+    /// <summary>
+    /// Unfreezes time and sends the user to the Level select scene. Sets bool to false
+    /// </summary>
     public void LoadLevel()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
         GameIsPaused = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(1);
