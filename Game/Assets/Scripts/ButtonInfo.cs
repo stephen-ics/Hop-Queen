78bf1c4d77478e7737e00b5ac379cc2c0893c003
stephen-ics <stephenni1234@gmail.com>
2022-12-07 20:44:42 -0500

    Basic Shop Layout (buggy)

---
+++ Game/Assets/Scripts/ButtonInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int ItemID;
+    public Text PriceTxt;
+    public Text QuantityTxt;
+    public GameObject ShopManager; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        PriceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManage>().shopItems[2, ItemID].ToString();
+        QuantityTxt.text = "Quantity: $" + ShopManager.GetComponent<ShopManage>().shopItems[3, ItemID].ToString();
+
+    }
+}
